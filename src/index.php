c658c5eee776cfdccb544b18a63f0f4ec088520b
Gustavo Jhon <hustavojhon@gmail.com>
2023-10-31 15:47:31 -0500

    mv index.php

--- src/index.php
+++ src/index.php
@@ -73,7 +73,7 @@
     $('#buscar').click(function () {
         var dni = $('#documento').val();
         $.ajax({
-            url: "./src/controller/consultas.php",
+            url: "controller/consultas.php",
             type: "POST",
             cache: false,
             data: { dni: dni },
